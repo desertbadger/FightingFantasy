d2c091af3eb326268fa0647a7b9463b78dea5ef3
Paddy <pmcconnell@godaddy.com>
2016-01-15 06:35:55 -0800

    added more templates, flushed out some CSS changes

--- FightingFantasy/run.py
+++ FightingFantasy/run.py
@@ -20,6 +20,25 @@ def connect_db():
 def home():
     return render_template('index.html')
 
+@app.route('/encounter')
+def encounter():
+    return render_template('encounter.html')
+
+@app.route('/books')
+def books():
+    return render_template('books.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run()
